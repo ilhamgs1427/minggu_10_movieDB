78cc79a11b21b86f3cb51d9f0e94ff761bd49f95
ilhamgs1427 <pp3583316@gmail.com>
2023-05-13 16:17:02 +0700

    upload flutter moviedb

---
+++ lib/data/api_provider.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:minggu_10_moviedb/model/popular_movies.dart';
+import 'package:http/http.dart' as http;
+
+class ApiProvider {
+  String apiKey = '0fc5740199faa752da813c8c97f659e8';
+  String baseUrl = 'https://api.themoviedb.org/3';
+
+  http.Client client = http.Client();
+
+  Future<PopularMovies?> getPopularMovies() async {
+    http.Response response =
+        await client.get('$baseUrl/movie/popular?api_key=$apiKey');
+    if (response.statusCode == 200) {
+      return PopularMovies.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
+}
